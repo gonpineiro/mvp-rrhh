ed84ccfbf51ccda59bab75429116bce4ee72d698
unknown <gon.pineiro@gmail.com>
2020-05-18 19:05:49 -0300

    Seeder Arreglado

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this->call(UsersSeeder::class);
+      DB::table('users')->insert([
+        'name' => 'administrador',
+        'email' => 'administrador@sab5.com.ar',
+        'password' => Hash::make('admin')]);    
+        
+          //ROLEs
+      Role::create(['name'  => 'Admin','slug'  => 'admin','special'  => 'all-access']);  //ADMIN ROLL
+      DB::table('role_user')->insert(['role_id' => '1','user_id' => 1]);
+      
+      DB::table('roles')->insert(['name'  => 'Supervisor operaciones'  ,'slug'  => 'supervisor'   ,'description'  => 'Supervisores operaciones']);
+      DB::table('roles')->insert(['name'  => 'Gerente de cuenta'  ,'slug'  => 'gerente.cuente'   ,'description'  => 'Gerente de cuenta']);
+      DB::table('roles')->insert(['name'  => 'RRHH'  ,'slug'  => 'rrhh'   ,'description'  => 'RRHH']);
+      DB::table('roles')->insert(['name'  => 'Facturación'  ,'slug'  => 'facturacion'   ,'description'  => 'Facturacion']);
+
+      DB::table('permissions')->insert(['name'  => 'Consultar supervisores'  ,'slug'  => 'consultar.supervisor','description'  => 'Consultar Supervisores']);
+      //SUPERVISORES
+      DB::table('permissions')->insert(['name'  => 'Consultar personal'  ,'slug'  => 'consultar.personal','description'  => 'Consultar personal']);
+      DB::table('permissions')->insert(['name'  => 'Reportar personal'  ,'slug'  => 'reportar.personal','description'  => 'Reportar personal']);
+      DB::table('permissions')->insert(['name'  => 'Consultar legajo'  ,'slug'  => 'consultar.legajo','description'  => 'Consultar legajo']);
+      
+      //GERENTES
+      DB::table('permissions')->insert(['name'  => 'Consultar personal sup'  ,'slug'  => 'personal.supervisores','description'  => 'Consultar personal sup']);
+      DB::table('permissions')->insert(['name'  => 'Consultar asginaciones'  ,'slug'  => 'consultar.asignaciones','description'  => 'Consultar asginaciones']);
+
+
+      DB::table('permissions')->insert(['name'  => 'RRHH'  ,'slug'  => 'rrhh'   ,'description'  => 'rrhh']);
+
+      DB::table('permissions')->insert(['name'  => 'Facturación'  ,'slug'  => 'facturacion'   ,'description'  => 'facturacion']);
+
+      DB::table('permissions')->insert(['name'  => 'Crear user supervisor'  ,'slug'  => 'crearuser.supervisor','description'  => 'Crear user Supervisor']);
+
+      DB::table('permission_role')->insert(['permission_id'  => 2  ,'role_id'  => 2]);
+      DB::table('permission_role')->insert(['permission_id'  => 3  ,'role_id'  => 2]);
+      DB::table('permission_role')->insert(['permission_id'  => 4  ,'role_id'  => 2]);
+      DB::table('permission_role')->insert(['permission_id'  => 6  ,'role_id'  => 2]);
+
+
+      DB::table('permission_role')->insert(['permission_id'  => 1  ,'role_id'  => 3]);
+      DB::table('permission_role')->insert(['permission_id'  => 2  ,'role_id'  => 3]);
+      DB::table('permission_role')->insert(['permission_id'  => 3  ,'role_id'  => 3]);
+      DB::table('permission_role')->insert(['permission_id'  => 4  ,'role_id'  => 3]);
+      DB::table('permission_role')->insert(['permission_id'  => 5  ,'role_id'  => 3]);
+      DB::table('permission_role')->insert(['permission_id'  => 6  ,'role_id'  => 3]);
+
+
+/*       $this->call(UsersSeeder::class);
       $this->call(RolesSeeder::class);
       $this->call(PermissionsSeeder::class);
-      $this->call(PermissionRoleSeeder::class);
+      $this->call(PermissionRoleSeeder::class); */
 
     }
 }
