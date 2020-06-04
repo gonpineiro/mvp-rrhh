9670f7b3f17705aae1141259ed0ee5020b64755a
Gonzalo Piñeiro <gon.pineiro@gmail.com>
2020-06-04 13:17:54 -0300

    Version Deploy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-/*       DB::table('users')->insert([
+      DB::table('users')->insert([
         'name' => 'administrador',
         'email' => 'administrador@sab5.com.ar',
         'password' => Hash::make('admin')]);    
@@ -60,7 +60,7 @@ class DatabaseSeeder extends Seeder
       DB::table('permission_role')->insert(['permission_id'  => 7  ,'role_id'  => 4]);
 
       DB::table('permission_role')->insert(['permission_id'  => 8  ,'role_id'  => 5]);
- */
+
 
       $this->call(UsersSeeder::class);
       $this->call(RolesSeeder::class);
